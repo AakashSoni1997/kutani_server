6bdd43d42da67291b8527962af0735cd58216ae1
aakash <asoni1940@gmail.com>
2023-02-06 23:47:05 +0530

    homepage api correction

--- controller/homeController.js
+++ controller/homeController.js
@@ -2,15 +2,17 @@ const Home=require("../models/homeModel");
 
 exports.homeController=async(req,res,next)=>{
     let images=[]
+
     // console.log(images,"homepage");
     if (typeof req.body.image === "string") {
         images.push(req.body.images);
       } else {
-        images = req.body.image;
+        images = req.body.images;
       }
 
     const home=await Home.create({
         images
     })
+    console.log(images,"homepage")
 res.status(201).json({success:true,home})
 }
